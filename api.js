7ca515444bd3a332115598e122ae6dcbe78a92ac
hafizhalareef <hafizhalareef26@gmail.com>
2020-11-18 11:07:42 +0700

    first commit

---
+++ api.js
@@ -0,0 +1,329 @@
+const data  = [
+    {
+      Judul: `Bersamamu`,
+      Artis: `Vierratale`,
+      Album: `My First Love`,
+      Lirik: `Memandang wajahmu cerah Membuatku tersenyum senang Indah dunia Tentu saja kita pernah Mengalami perbedaan Kita lalui Tapi aku merasa Jatuh terlalu dalam cintamu Ku tak akan berubah Ku tak ingin kau pergi Selamanya... Ku kan setia menjagamu Bersama dirimu, dirimu, oh Sampai nanti akan selalu Bersama dirimu... Saat bersamamu kasih Ku merasa bahagia Dalam pelukmu Tapi aku merasa jatuh terlalu dalam cintamu Ku tak akan berubah Ku tak ingin kau pergi Selamanya oohh... Ku kan setia menjagamu Bersama dirimu, dirimu oh... Sampai nanti akan selalu Bersama dirimu Seperti yang kau katakan Kau akan selalu ada Menjaga, memeluk diriku dengan cintamu Dengan cintamu... Ku kan setia menjagamu Bersama dirimu, dirimu oh Sampai nanti Akan selalu Bersama dirimu... Saat bersamamu kasih ku merasa bahagia Dalam pelukmu...`,
+    },
+    {
+      Judul: `Dengarkan Curhatku`,
+      Artis: `Vierratale`,
+      Album: `My First Love`,
+      Lirik: `Dengarkan curhatku Tentang dirinya Betapa anehnya Tingkah lakunya Jujurlah padaku, jujurlah padaku Kau menyimpan rasa, kau menyimpan rasa cinta
+  Nyatakan padaku, nyatakan padaku
+  Perasaan itu, perasaan itu cinta
+  Dengarkan curhatku
+  Tentang dirinya
+  Betapa manisnya
+  Senyum bibirnya
+  Jujurlah padaku, jujurlah padaku
+  Kau menyimpan rasa, kau menyimpan rasa cinta
+  Nyatakan padaku, nyatakan padaku
+  Perasaan itu, perasaan itu cinta
+  Dengarkan curhatku
+  Jujurlah padaku, jujurlah padaku
+  Kau menyimpan rasa, kau menyimpan rasa cinta
+  Nyatakan padaku, nyatakan padaku
+  Perasaan itu, perasaan itu cinta
+  Dengarkan curhatku
+  Tentang dirinya`,
+    },
+    {
+      Judul: `Menjaga Hati`,
+      Artis: `Yovie & Nuno`,
+      Album: `The Special One`,
+      Lirik: `Masih tertinggal bayanganmu
+              Yang telah membekas di relung hatiku
+              Hujan tanpa henti seolah pertanda
+  Cinta tak di sini lagi, kau telah berpaling
+  Biarkan aku menjaga perasaan ini
+  Menjaga segenap cinta yang telah kau beri
+  Engkau pergi, aku takkan pergi
+  Kau menjauh, aku takkan jauh
+  Sebenarnya diriku masih mengharapkanmu
+  Masih adakah cahaya rindumu
+  Yang dulu selalu cerminkan hatimu
+  Aku takkan bisa menghapus dirimu
+  Meski kulihat kini kau di seberang sana
+  Biarkan aku menjaga perasaan ini
+  Menjaga segenap cinta yang telah kau beri
+  Engkau pergi, aku takkan pergi
+  Kau menjauh, aku takkan jauh
+  Sebenarnya diriku masih mengharapkanmu
+  Andai akhirnya kau tak juga kembali
+  Aku tetap sendiri menjaga hati
+  Engkau pergi, aku takkan pergi
+  Kau menjauh, aku takkan jauh
+  Sebenarnya diriku masih mengharapkanmu
+  Biarkan aku menjaga perasaan ini
+  Menjaga segenap cinta yang telah kau beri
+  Engkau pergi, aku takkan pergi
+  Kau menjauh, aku takkan jauh
+  Sebenarnya diriku masih mengharapkanmu
+  Sejujurnya diriku masih mengharapkanmu`,
+    },
+    {
+      Judul: `Dia Milikku`,
+      Artis: `Yovie & Nuno`,
+      Album: `The Special One`,
+      Lirik: `Semula ku tak tahu
+  Engkau juga kan ingin memilikinya
+  Bukankah ku lebih dulu
+  Bila engkau temanku
+  Sebaiknya tak mengganggu
+  Dia untukku
+  Bukan untukmu
+  Dia milikku
+  Bukan milikmu
+  Pergilah kamu
+  Jangan kau ganggu
+  Biarkan aku
+  Mendekatinya
+  Kamu
+  Tak akan mungkin
+  Mendapatkannya
+  Karena dia
+  Berikan aku
+  Pertanda cinta
+  Janganlah kamu banyak bermimpi, oh
+  Dia Untuk aku
+  Bukankah belum pasti
+  Kamu juga kan jadi
+  Dengan dirinya
+  Dia yang menentukan
+  Apa yang kan terjadi
+  Tak usah mengaturku
+  Dia untukku
+  Bukan untukmu
+  Dia milikku
+  Bukan milikmu
+  Lihatlah nanti
+  Lihatlah saja
+  Biarkan aku
+  Mendekatinya
+  Kamu
+  Tak akan mungkin
+  Mendapatkannya
+  Karena dia
+  Berikan aku
+  Pertanda juga
+  Janganlah kamu banyak bermimpi ooh
+  Kusarankan engkau mundur saja, ooh
+  Dia untukku
+  Bukan untukmu
+  Dia milikku
+  Bukan milikmu
+  Pergilah kamu
+  Jangan kau ganggu
+  Biarkan aku
+  Mendekatinya`,
+    },
+    {
+      Judul: `Red`,
+      Artis: `Taylor Swift`,
+      Album: `Red`,
+      Lirik: `Loving him is like
+  Driving a new Maserati down a dead-end street<br>
+  Faster than the wind<br>
+  Passionate as sin<br>
+  Ending so suddenly<br>
+  Loving him is like<br>
+  Trying to change your mind once you're already flying through the free fall<br>
+  Like the colors in autumn, so bright<br>
+  Just before they lose it all<br>
+  Losing him was blue like I'd never known<br>
+  Missing him was dark grey, all alone<br>
+  Forgetting him was like trying to know somebody you never met<br>
+  But loving him was red (re-e-e-ed, re-e-e-ed)<br>
+  (Re-e-e-ed, re-e-e-ed)<br>
+  Loving him was red (re-e-e-ed, re-e-e-ed)<br>
+  (Re-e-e-ed, re-e-e-ed)<br>
+  Touching him was like<br>
+  Realizing all you ever wanted was right there in front of you<br>
+  Memorizing him was as<br>
+  Easy as knowing all the words to your old favorite song<br>
+  Fighting with him was like<br>
+  Trying to solve a crossword and realizing there's no right answer<br>
+  Regretting him was like<br>
+  Wishing you never found out that love could be that strong<br>
+  Losing him was blue like I'd never known<br>
+  Missing him was dark grey, all alone<br>
+  Forgetting him was like trying to know somebody you never met<br>
+  But loving him was red (re-e-e-ed, re-e-e-ed)<br>
+  Oh, red (re-e-e-ed, re-e-e-ed)<br>
+  Burning red (re-e-e-ed, re-e-e-ed)<br>
+  (Re-e-e-ed, re-e-e-ed)<br>
+  Remembering him comes in flashbacks<br>
+  And echoes<br>
+  Tell myself it's time now<br>
+  Gotta let go<br>
+  But moving on from him is impossible<br>
+  When I still see it all in my head<br>
+  In burning red<br>
+  Burning it was red<br>
+  Oh, losing him was blue like I'd never known<br>
+  Missing him was dark grey, all alone<br>
+  Forgetting him was like trying to know somebody you never met<br>
+  'Cause loving him was red (re-e-e-ed, re-e-e-ed)<br>
+  Yeah, yeah, red (re-e-e-ed, re-e-e-ed)<br>
+  Oh, burning red (re-e-e-ed, re-e-e-ed)<br>
+  (Re-e-e-ed, re-e-e-ed)<br>
+  And that's why he's spinning 'round in my head (re-e-e-ed, re-e-e-ed)<br>
+  Comes back to me, burning red (re-e-e-ed, re-e-e-ed)<br>
+  Yeah, yeah (re-e-e-ed, re-e-e-ed)<br>
+  His love was like<br>
+  Driving a new Maserati down a dead-end street`,
+    },
+    {
+      Judul: `Love Story`,
+      Artis: `Taylor Swift`,
+      Album: `Fearless`,
+      Lirik: `We were both young when I first saw you
+  I close my eyes and the flashback starts
+  I'm standin' there
+  On a balcony in summer air
+  See the lights, see the party, the ball gowns
+  See you make your way through the crowd
+  And say, Hello
+  Little did I know
+  That you were Romeo, you were throwin' pebbles
+  And my daddy said, Stay away from Juliet
+  And I was cryin' on the staircase
+  Beggin' you, Please don't go,  and I said
+  Romeo, take me somewhere we can be alone
+  I'll be waiting, all there's left to do is run
+  You'll be the prince and I'll be the princess
+  It's a love story, baby, just say, Yes
+  So I sneak out to the garden to see you
+  We keep quiet, 'cause we're dead if they knew
+  So close your eyes
+  Escape this town for a little while, oh oh
+  'Cause you were Romeo, I was a scarlet letter
+  And my daddy said, Stay away from Juliet
+  But you were everything to me
+  I was beggin' you, Please don't go, and I said
+  Romeo, take me somewhere we can be alone
+  I'll be waiting, all there's left to do is run
+  You'll be the prince and I'll be the princess
+  It's a love story, baby, just say, Yes
+  Romeo, save me, they're tryna tell me how to feel
+  This love is difficult, but it's real
+  Don't be afraid, we'll make it out of this mess
+  It's a love story, baby, just say, Yes
+  Oh, oh
+  I got tired of waiting
+  Wonderin' if you were ever comin' around
+  My faith in you was fading
+  When I met you on the outskirts of town, and I said
+  Romeo, save me, I've been feeling so alone
+  I keep waiting for you, but you never come
+  Is this in my head? I don't know what to think
+  He knelt to the ground and pulled out a ring
+  And said, Marry me, Juliet
+  You'll never have to be alone
+  I love you and that's all I really know
+  I talked to your dad, go pick out a white dress
+  It's a love story, baby, just say, Yes
+  Oh, oh, oh
+  Oh, oh, oh, oh
+  'Cause we were both young when I first saw you`,
+    },
+    {
+      Judul: `White Horse`,
+      Artis: `Taylor Swift`,
+      Album: `Fearless`,
+      Lirik: `Say you're sorry That face of an angel
+  Comes out just when you need it to
+  As I paced back and forth all this time
+  'Cause I honestly believed in you
+  Holding on
+  The days drag on
+  Stupid girl,
+  I should have known, I should have known
+  That I'm not a princess, this ain't a fairy tale
+  I'm not the one you'll sweep off her feet,
+  Lead her up the stairwell
+  This ain't Hollywood, this is a small town,
+  I was a dreamer before you went and let me down
+  Now it's too late for you
+  And your white horse, to come around
+  Maybe I was naive,
+  Got lost in your eyes
+  And never really had a chance
+  My mistake I didn't know to be in love
+  You had to fight to have the upper hand
+  I had so many dreams
+  About you and me
+  Happy endings
+  Now I know
+  I'm not a princess, this ain't a fairy tale
+  I'm not the one you'll sweep off her feet,
+  Lead her up the stairwell
+  This ain't Hollywood, this is a small town,
+  I was a dreamer before you went and let me down
+  Now it's too late for you
+  And your white horse, to come around
+  And there you are on your knees,
+  Begging for forgiveness, begging for me
+  Just like I always wanted but I'm so sorry
+  'Cause I'm not your princess, this ain't a fairytale
+  I'm gonna find someone someday who might actually treat me well
+  This is a big world, that was a small town
+  There in my rear view mirror disappearing now
+  And its too late for you and your white horse
+  Now its too late for you and your white horse, to catch me now
+  Oh, whoa, whoa, whoa
+  Try and catch me now
+  Oh, it's too late
+  To catch me now`,
+    },
+    {
+      Judul: `Someone Like You`,
+      Artis: `Adele`,
+      Album: `21`,
+      Lirik: `I heard that you're settled down
+      That you found a girl and you're married now
+  I heard that your dreams came true
+  Guess she gave you things, I didn't give to you
+  Old friend, why are you so shy?
+  Ain't like you to hold back or hide from the light
+  I hate to turn up out of the blue, uninvited
+  But I couldn't stay away, I couldn't fight it
+  I had hoped you'd see my face
+  And that you'd be reminded that for me, it isn't over
+  Never mind, I'll find someone like you
+  I wish nothing but the best for you, too
+  Don't forget me,  I beg
+  I remember you said
+  Sometimes it lasts in love, but sometimes it hurts instead
+  Sometimes it lasts in love, but sometimes it hurts instead
+  You know how the time flies
+  Only yesterday was the time of our lives
+  We were born and raised in a summer haze
+  Bound by the surprise of our glory days
+  I hate to turn up out of the blue, uninvited
+  But I couldn't stay away, I couldn't fight it
+  I had hoped you'd see my face
+  And that you'd be reminded that for me, it isn't over
+  Never mind, I'll find someone like you
+  I wish nothing but the best for you, too
+  Don't forget me, I begged
+  I remember you said
+  Sometimes it lasts in love, but sometimes it hurts instead
+  Nothing compares, no worries or cares
+  Regrets and mistakes, they're memories made
+  Who would have known how bittersweet this would taste?
+  Never mind, I'll find someone like you
+  I wish nothing but the best for you
+  Don't forget me,  I beg
+  I remember you said
+  Sometimes it lasts in love, but sometimes it hurts instead
+  Never mind, I'll find someone like you
+  I wish nothing but the best for you, too
+  Don't forget me, I begged
+  I remember you said
+  Sometimes it lasts in love, but sometimes it hurts instead
+  Sometimes it lasts in love, but sometimes it hurts instead`,
+    },
+  ];
+  
